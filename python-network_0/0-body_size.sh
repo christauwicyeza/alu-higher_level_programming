c489e5606158f48f11d4f7015799fbe5daf110ff
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2022-11-15 13:55:13 +0200

    python

--- python-network_0/0-body_size.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#sends a request to that URL, and displays the size of the body of the response
-curl -sI "$1" | grep 'Content-Length:' | cut -f2 -d' '
--- python-network_0/4-header.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# sends arguments using get
-curl -sG "$1" -H "X-School-User-Id: 98"
