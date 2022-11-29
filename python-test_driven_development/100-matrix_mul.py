68a1940a87df48b32b9eef36c9863507645517c7
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2022-11-29 08:47:49 +0200

    python

--- python-test_driven_development/100-matrix_mul.py
+++ python-test_driven_development/100-matrix_mul.py
@@ -1,56 +1,54 @@
 #!/usr/bin/python3
-"""Defines a matrix multiplication function."""
+"""matrix multiplication module"""
 
 
 def matrix_mul(m_a, m_b):
-    """Multiply two matrices.
-    """
-
-    if m_a == [] or m_a == [[]]:
-        raise ValueError("m_a can't be empty")
-    if m_b == [] or m_b == [[]]:
-        raise ValueError("m_b can't be empty")
-
-    if not isinstance(m_a, list):
+    """multiply two matrices"""
+    if type(m_a) != list:
         raise TypeError("m_a must be a list")
-    if not isinstance(m_b, list):
+    elif type(m_b) != list:
         raise TypeError("m_b must be a list")
+    elif len(m_a) < 1:
+        raise ValueError("m_a can't be empty")
+    elif len(m_b) < 1:
+        raise ValueError("m_b can't be empty")
+    for i in m_a:
+        if type(i) != list:
+            raise TypeError("m_a must be a list of lists")
+    for i in m_a:
+        if len(i) < 1:
+            raise ValueError("m_a can't be empty")
+    for i in m_a:
+        for el in i:
+            if type(el) != int and type(el) != float:
+                raise TypeError("m_a should contain only integers or floats")
+    for i in m_a:
+        if len(i) != len(m_a[0]):
+            raise TypeError("each row of m_a must be of the same size")
 
-    if not all(isinstance(row, list) for row in m_a):
-        raise TypeError("m_a must be a list of lists")
-    if not all(isinstance(row, list) for row in m_b):
-        raise TypeError("m_b must be a list of lists")
-
-    if not all((isinstance(ele, int) or isinstance(ele, float))
-               for ele in [num for row in m_a for num in row]):
-        raise TypeError("m_a should contain only integers or floats")
-    if not all((isinstance(ele, int) or isinstance(ele, float))
-               for ele in [num for row in m_b for num in row]):
-        raise TypeError("m_b should contain only integers or floats")
-
-    if not all(len(row) == len(m_a[0]) for row in m_a):
-        raise TypeError("each row of m_a must should be of the same size")
-    if not all(len(row) == len(m_b[0]) for row in m_b):
-        raise TypeError("each row of m_b must should be of the same size")
-
+    for j in m_b:
+        if type(j) != list:
+            raise TypeError("m_b must be a list of lists")
+    for j in m_b:
+        if len(j) < 1:
+            raise ValueError("m_b can't be empty")
+    for j in m_b:
+        for el_2 in j:
+            if type(el_2) != int and type(el_2) != float:
+                raise TypeError("m_b should contain only integers or floats")
+    for j in m_b:
+        if len(j) != len(m_b[0]):
+            raise TypeError("each row of m_b must be of the same size")
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
-
-    inverted_b = []
-    for r in range(len(m_b[0])):
-        new_row = []
-        for c in range(len(m_b)):
-            new_row.append(m_b[c][r])
-        inverted_b.append(new_row)
-
-    new_matrix = []
-    for row in m_a:
-        new_row = []
-        for col in inverted_b:
-            prod = 0
-            for i in range(len(inverted_b[0])):
-                prod += row[i] * col[i]
-            new_row.append(prod)
-        new_matrix.append(new_row)
-
-    return new_matrix
+    result = []
+    for n in m_a:
+        temp_res = []
+        for i in range(len(m_b[0])):
+            temp = [j[i] for j in m_b]
+            addition = 0
+            for k, l in zip(n, temp):
+                addition += k * l
+            temp_res.append(addition)
+        result.append(temp_res)
+    return result
--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Defines a matrix multiplication function using NumPy."""
-import numpy as np
+"""
+Defines lazy_matrix function
+"""
+import numpy
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """Return the multiplication of two matrices.
-    """
-
-    return (np.matmul(m_a, m_b))
+    """ calculates the matrix multiplication of two matrices"""
+    return numpy.matmul(m_a, m_b)
