08aa7e0b17077f6a5a856bfa81388bb1b9895b31
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2023-02-02 12:36:36 +0200

    JS

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 const arg = process.argv[2];
 if (isNaN(arg)) {
-console.log('Not a number');
+  console.log('Not a number');
 } else {
-console.log('My number: ' + arg);
+  console.log('My number: ' + arg);
 }
