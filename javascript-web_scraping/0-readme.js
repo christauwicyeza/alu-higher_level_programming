2728e151dde94b1081bae9717e872c69add10ad4
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2023-02-06 13:35:32 +0200

    json

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (error, data) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log(data);
+});
